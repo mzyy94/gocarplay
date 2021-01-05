2d696479c6563438f35691a3226404fa788a6143
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-01-06 01:40:01 +0900

    Remove common Header

--- protocol/structures.go
+++ protocol/structures.go
@@ -1,7 +1,6 @@
 package protocol
 
 type SendFile struct {
-	Header       `struc:"skip"`
 	FileNameSize int32 `struc:"int32,little,sizeof=FileName"`
 	FileName     string
 	ContentSize  int32 `struc:"int32,little,sizeof=Content"`
@@ -9,7 +8,6 @@ type SendFile struct {
 }
 
 type Open struct {
-	Header         `struc:"skip"`
 	Width          int32 `struc:"int32,little"`
 	Height         int32 `struc:"int32,little"`
 	VideoFrameRate int32 `struc:"int32,little"`
@@ -20,48 +18,39 @@ type Open struct {
 }
 
 type Heartbeat struct {
-	Header `struc:"skip"`
 }
 
 type ManufacturerInfo struct {
-	Header `struc:"skip"`
-	A      int32 `struc:"int32,little"`
-	B      int32 `struc:"int32,little"`
+	A int32 `struc:"int32,little"`
+	B int32 `struc:"int32,little"`
 }
 
 type CarPlay struct {
-	Header `struc:"skip"`
-	Type   CarPlayType `struc:"int32,little"`
+	Type CarPlayType `struc:"int32,little"`
 }
 
 type SoftwareVersion struct {
-	Header  `struc:"skip"`
 	Version string `struc:"[32]byte"`
 }
 
 type BluetoothAddress struct {
-	Header  `struc:"skip"`
 	Address string `struc:"[17]byte"`
 }
 
 type BluetoothPIN struct {
-	Header  `struc:"skip"`
 	Address string `struc:"[4]byte"`
 }
 
 type Plugged struct {
-	Header    `struc:"skip"`
 	PhoneType int  `struc:"int32,little"`
 	Wifi      bool `struc:"int32,little"`
 	// FIXME: Send WifiParam only when no wifi is ok
 }
 
 type Unplugged struct {
-	Header `struc:"skip"`
 }
 
 type VideoData struct {
-	Header   `struc:"skip"`
 	Width    int32  `struc:"int32,little"`
 	Height   int32  `struc:"int32,little"`
 	Flags    int32  `struc:"int32,little"`
@@ -71,7 +60,6 @@ type VideoData struct {
 }
 
 type AudioData struct {
-	Header         `struc:"skip"`
 	DecodeType     int32        `struc:"int32,little"`
 	Volume         float32      `struc:"float32,little"`
 	AudioType      AudioType    `struc:"int32,little"`
@@ -81,7 +69,6 @@ type AudioData struct {
 }
 
 type Touch struct {
-	Header `struc:"skip"`
 	Action TouchAction `struc:"int32,little"`
 	X      uint32      `struc:"uint32,little"`
 	Y      uint32      `struc:"uint32,little"`
@@ -89,11 +76,9 @@ type Touch struct {
 }
 
 type MultiTouch struct {
-	Header `struc:"skip"`
 	// TODO: Implement
 }
 
 type Unknown struct {
-	Header `struc:"skip"`
-	Data   []byte `struc:"skip"`
+	Data []byte `struc:"skip"`
 }
