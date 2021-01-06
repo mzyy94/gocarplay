0585aa439ba8a5e060d63c36e1d9c53fc2dffb9d
Yuki MIZUNO <mizuyuu0904@gmail.com>
2021-01-06 22:37:04 +0900

    Add Start func of communication

--- link/communicate.go
+++ link/communicate.go
@@ -1,9 +1,13 @@
 package link
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"time"
 
 	"github.com/google/gousb"
+	"github.com/mzyy94/gocarplay/protocol"
 )
 
 var epIn *gousb.InEndpoint
@@ -19,6 +23,28 @@ func Init() error {
 	}
 	ctx = context.Background()
 	return nil
+}
+
+func intToByte(data int32) []byte {
+	var buf bytes.Buffer
+	binary.Write(&buf, binary.LittleEndian, data)
+	return buf.Bytes()
+}
+
+func Start(width, height, fps, dpi int32) {
+	SendData(&protocol.SendFile{FileName: "/tmp/screen_dpi\x00", Content: intToByte(dpi)})
+	SendData(&protocol.Open{Width: width, Height: height, VideoFrameRate: fps, Format: 5, PacketMax: 4915200, IBoxVersion: 2, PhoneWorkMode: 2})
+
+	SendData(&protocol.ManufacturerInfo{A: 0, B: 0})
+	SendData(&protocol.SendFile{FileName: "/tmp/night_mode\x00", Content: intToByte(1)})
+	SendData(&protocol.SendFile{FileName: "/tmp/hand_drive_mode\x00", Content: intToByte(1)})
+	SendData(&protocol.SendFile{FileName: "/tmp/charge_mode\x00", Content: intToByte(0)})
+	SendData(&protocol.SendFile{FileName: "/tmp/box_name\x00", Content: bytes.NewBufferString("BoxName").Bytes()})
+
+	for {
+		SendData(&protocol.Heartbeat{})
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func Communicate(onData func(interface{}), onError func(error)) {
